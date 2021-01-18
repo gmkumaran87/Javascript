b073f29ec722668fe829a1ed60ac1713aaa9abd0
Muthukumaran <gmkumaran87@gmail.com>
2021-01-18 18:43:52 +0530

    Weather App changes

---
+++ beginner-javascript/exercises/Weather-App/app.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('.form-control');
+const search = document.getElementById('tempSearch');
+
+const weather = new Weather();
+const ui = new UI();
+
+// Add Event Listener
+search.addEventListener('click', getUpdates);
+
+// Event Listener for Farenheit
+const faren = document.querySelector('.farenheit');
+console.log(faren);
+
+faren.addEventListener('click', function() {
+    console.log(this);
+    this.classList.add('selected');
+    this.classList.add('bordered');
+    setTimeout(() => {
+        console.log(this);
+        this.classList.remove('bordered');
+    }, 2000);
+    ui.tempChange;
+});
+
+function getUpdates(e) {
+    e.preventDefault();
+
+    const city = form.value;
+
+    if (city !== '') {
+        weather.getWeather(city).then((data) => {
+            console.log(data);
+            // Displaying the Temperature in the UI
+            ui.displayTemp(data);
+        });
+    }
+}
