feb7601fee1c883d3454f93cbf856bc62b16a6aa
Muthukumaran <gmkumaran87@gmail.com>
2021-01-22 18:10:27 +0530

    New changes

--- beginner-javascript/exercises/Weather-App/weather.js
+++ beginner-javascript/exercises/Weather-App/weather.js
@@ -2,13 +2,13 @@ class Weather {
     constructor(city, country) {
         this.city = city;
         this.country = country;
-        this.appId = '37a4f98562cccd79e08032830a635a0a';
+        this.appId = '';
         this.faranheit = 273.15;
     }
 
     async getWeather() {
         const temp = await fetch(
-            `http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.appId}`
+            `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.country}&APPID=${this.appId}`
         );
 
         const tempData = await temp.json();
